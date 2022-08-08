2dee6fd9057fa149e9c45a00e125b696deea6a81
TrucXNguyen <nguyenxuantruc30111997@gmail.com>
2022-08-08 15:30:11 +0700

    [TrucNguyen] init

---
+++ src/main/java/com/practice/b/service/BServiceApplication.java
@@ -0,0 +1,13 @@
+package com.practice.b.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BServiceApplication.class, args);
+	}
+
+}
